e1130a0624c7ede3a2710eb66acc57b9212518bb
Jhadevloper4035 <navrojjha21@gmail.com>
2026-03-18 23:26:00 +0800

    polish footer

--- frontend/data/footerLinks.js
+++ frontend/data/footerLinks.js
@@ -1,10 +1,7 @@
 export const socialLinks = [
   { href: "#", className: "social-facebook", iconClass: "icon-fb" },
-  { href: "#", className: "social-twiter", iconClass: "icon-x" },
   { href: "#", className: "social-instagram", iconClass: "icon-instagram" },
-  { href: "#", className: "social-tiktok", iconClass: "icon-tiktok" },
-  { href: "#", className: "social-amazon", iconClass: "icon-amazon" },
-  { href: "#", className: "social-pinterest", iconClass: "icon-pinterest" },
+  { href: "#", className: "social-whatsapp", iconClass: "icon-whatsapp" },
 ];
 
 export const footerLinks = [
